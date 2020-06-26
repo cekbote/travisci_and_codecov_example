fca13a255497d02ca868f2d4be8fdace29b83c31
Chanakya-Ekbote <ca10@iitbbs.ac.in>
2020-06-26 23:02:31 +0530

    change
    Signed-off-by: Chanakya-Ekbote <ca10@iitbbs.ac.in>

--- travisci_and_codecov_example/tests/test_addition.py
+++ travisci_and_codecov_example/tests/test_addition.py
@@ -6,7 +6,7 @@ from travisci_and_codecov_example.src.addition import add
 class TestAddition(unittest.TestCase):
 
     def test_add(self):
-        self.assertEqual(add(5, 6), 11)
+        self.assertEqual(add(5, 6), 13)
 
 
 if '__name__' == '__main__':
