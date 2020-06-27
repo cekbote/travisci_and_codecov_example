f610eed578f987223dd266dcef064ad5fbe1523e
Chanakya-Ekbote <ca10@iitbbs.ac.in>
2020-06-27 15:52:04 +0530

    Test for subtract
    Signed-off-by: Chanakya-Ekbote <ca10@iitbbs.ac.in>

--- travisci_and_codecov_example/tests/test_addition.py
+++ travisci_and_codecov_example/tests/test_addition.py
@@ -8,6 +8,9 @@ class TestAddition(unittest.TestCase):
     def test_add(self):
         self.assertEqual(add(5, 6), 11)
 
+    def test_sub(self):
+        self.assertEqual(sub(6, 5), 1)
+
 
 if '__name__' == '__main__':
     unittest.main()
