9a60fcdac3aabcf0ac11491d489431e96b510a1f
Chanakya-Ekbote <ca10@iitbbs.ac.in>
2020-07-06 01:07:37 +0530

    Testing Elasticsearch in Travis
    Signed-off-by: Chanakya-Ekbote <ca10@iitbbs.ac.in>

--- travisci_and_codecov_example/tests/test_addition.py
+++ travisci_and_codecov_example/tests/test_addition.py
@@ -1,11 +1,18 @@
 import unittest
 import sys
+from elasticsearch import Elasticsearch
 from travisci_and_codecov_example.src.addition import add, sub
 
 
 class TestAddition(unittest.TestCase):
 
+    def setUp(self):
+        self.es = Elasticsearch()
+        self.es.index(index='test', id=1, body={'test': 'test'})
+        print(self.es.get(index='test', id=1)['_source'])
+
     def test_add(self):
+
         self.assertEqual(add(5, 6), 11)
 
     def test_sub(self):
