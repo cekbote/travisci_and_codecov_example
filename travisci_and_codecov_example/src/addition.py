b923a1e3a4c1da9130ce138e0d94b004ac7c8391
Chanakya-Ekbote <ca10@iitbbs.ac.in>
2020-07-06 00:46:52 +0530

    WhoIS API integration test.
    Signed-off-by: Chanakya-Ekbote <ca10@iitbbs.ac.in>

--- travisci_and_codecov_example/src/addition.py
+++ travisci_and_codecov_example/src/addition.py
@@ -1,6 +1,22 @@
+from urllib.request import urlopen
+import json
+
+
 def add(a, b):
     return a + b
 
 
 def sub(a, b):
     return a - b
+
+def update_ip_info():
+
+    domainName = 'whoisxmlapi.com';
+    apiKey = 'Your API key'
+
+    url = 'https://www.whoisxmlapi.com/whoisserver/WhoisService?' \
+          + 'domainName=' + domainName + '&apiKey=' + apiKey + \
+          "&outputFormat=JSON" + "&ip=1"
+
+    data = urlopen(url).read().decode('utf8')
+    print(type(json.loads(data)))
